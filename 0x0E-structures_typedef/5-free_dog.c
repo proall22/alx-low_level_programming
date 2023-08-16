d70d20fde5f7aed5ec44eea3c9c307bf92d4583f
proall22 <allaboutpro22@gmail.com>
2023-08-16 22:56:02 +0300

    structure

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,18 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
-*free_dog - Frees dog
-*@d: dog to be freed
-*
-*/
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
-if (d == NULL)
-return;
-
-free(d->name);
-free(d->owner);
-free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
