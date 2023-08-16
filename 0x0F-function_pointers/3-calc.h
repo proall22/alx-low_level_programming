9a8d9595e66610618458f2a888c2ede32002c78f
proall22 <allaboutpro22@gmail.com>
2023-08-16 22:41:27 +0300

    function pointer task in alx

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,5 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
  *
@@ -21,5 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif
